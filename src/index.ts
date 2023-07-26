6464e52a1e784bd6b8b77deb33f251e5c18f2c3f
SuperAuguste <19855629+SuperAuguste@users.noreply.github.com>
2023-07-26 23:57:43 +0200

    Integration account-first refactor

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,13 @@
 import bodyParser from 'body-parser'
-import {
-    ActionRowBuilder,
-    AnyThreadChannel,
-    ButtonBuilder,
-    ButtonStyle,
-    ChannelType,
-    Client,
-    ComponentType,
-    GatewayIntentBits,
-    InteractionType,
-    MessageType,
-} from 'discord.js'
 import dotenv from 'dotenv'
 import express from 'express'
 
-import { ChannelbackRequest, ExternalResource } from './zendesk'
+import { Bot, createBot } from './bot'
+import { ChannelbackRequest, ExternalResource, Metadata } from './zendesk'
 
 dotenv.config()
 
-if (
-    !process.env.SITE ||
-    !process.env.DISCORD_TOKEN ||
-    !process.env.ZENDESK_EMAIL ||
-    !process.env.ZENDESK_TOKEN ||
-    !process.env.ZENDESK_REMOTE ||
-    !process.env.SUPPORT_CHANNEL_ID
-) {
+if (!process.env.SITE) {
     console.log('bad config')
     process.exit(1)
 }
@@ -34,7 +16,11 @@ const site = process.env.SITE!
 
 const app = express()
 
-let externalResourceQueue: ExternalResource[] = []
+/**
+ * Map from randomly generated id to bot
+ */
+let bots = new Map<string, Bot>()
+let botExternalResourceQueue = new Map<string, ExternalResource[]>()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -60,29 +46,62 @@ app.get('/manifest.json', (req, res) => {
     })
 })
 
+app.get('/preview-create-account', (req, res) => {
+    res.render('create-account.ejs', {
+        return_url: '#',
+    })
+})
+
 /**
  * @see https://developer.zendesk.com/documentation/channel_framework/understanding-the-channel-framework/administrative_interface/
  */
 app.post('/admin', (req, res) => {
-    // xss lol; not an actual issue in this context but still
-
-    // this hack exists because you need an account to associate with a pull request
-    // in the future we could properly configure discord interaction capturing with this
-    // but that's overkill for now
-
-    res.send(
-        `<form action="${req.body.return_url}" method="post"><input name="name" value="testacct1"><input name="metadata" value="none"><input name="state" value="none"><input type="submit"></form>`
-    )
-
-    console.error(req.body)
+    res.render('create-account.ejs', { return_url: req.body.return_url })
 })
 
 /**
  * @see https://developer.zendesk.com/documentation/channel_framework/understanding-the-channel-framework/pull_endpoint/
  */
 app.all('/pull', (req, res) => {
+    if (typeof req.body.metadata !== 'string') {
+        res.status(400).send('Bad request')
+        return
+    }
+
+    let metadata: Metadata
+    try {
+        metadata = JSON.parse(req.body.metadata)
+    } catch {
+        res.status(400).send('Bad request')
+        return
+    }
+
+    if (
+        typeof metadata.uuid !== 'string' ||
+        typeof metadata.token !== 'string' ||
+        typeof metadata.channel !== 'string'
+    ) {
+        res.status(400).send('Bad request')
+        return
+    }
+
+    if (!bots.has(metadata.uuid)) {
+        botExternalResourceQueue.set(metadata.uuid, [])
+        bots.set(
+            metadata.uuid,
+            createBot({
+                token: metadata.token,
+                supportChannelId: metadata.channel,
+                async pushExternalResource(resource: ExternalResource): Promise<void> {
+                    botExternalResourceQueue.get(metadata.uuid)!.push(resource)
+                },
+            })
+        )
+        console.log(`Configured bot that handles channel #${metadata.channel}`)
+    }
+
     res.send({
-        external_resources: externalResourceQueue.splice(0),
+        external_resources: botExternalResourceQueue.get(metadata.uuid)?.splice(0) ?? [],
     })
 })
 
@@ -90,32 +109,34 @@ app.all('/pull', (req, res) => {
  * @see https://developer.zendesk.com/documentation/channel_framework/understanding-the-channel-framework/channelback/
  */
 app.post('/channelback', async (req, res) => {
-    if (typeof req.body.message !== 'string' || typeof req.body.thread_id !== 'string') {
+    if (
+        typeof req.body.message !== 'string' ||
+        typeof req.body.thread_id !== 'string' ||
+        typeof req.body.metadata !== 'string' ||
+        // Check metadata characteristics
+        typeof req.body.metadata.uuid !== 'string' ||
+        typeof req.body.metadata.token !== 'string' ||
+        typeof req.body.metadata.channel !== 'string'
+    ) {
         res.status(400).send('Bad request')
         return
     }
 
-    const request = req.body as ChannelbackRequest
-
-    const channel = await client.channels.fetch(SUPPORT_CHANNEL_ID)
-    if (channel?.type !== ChannelType.GuildForum) {
+    const bot = bots.get(req.body.metadata.uuid)
+    if (!bot) {
         res.status(500).send('Internal server error')
         return
     }
 
-    const thread = await channel.threads.fetch(request.thread_id)
-    if (!thread) {
+    const externalId = await bot.channelback(req.body as ChannelbackRequest)
+
+    if (!externalId) {
         res.status(500).send('Internal server error')
         return
     }
-
-    const message = await thread.send({
-        content: request.message,
-        // files: request.file_urls,
-    })
 
     res.send({
-        external_id: `${thread.id}-${message.id}`,
+        external_id: externalId,
         allow_channelback: true,
     })
 })
@@ -129,178 +150,22 @@ app.get('/clickthrough', async (req, res) => {
         return
     }
 
-    const parts = req.query.external_id.split('-')
+    // We don't use a Promise.all here to avoid potential rate-limiting
+    for (const value of bots.values()) {
+        const location = await value.clickthrough({
+            external_id: req.query.external_id,
+        })
 
-    const channel = await client.channels.fetch(SUPPORT_CHANNEL_ID)
-    if (channel?.type !== ChannelType.GuildForum) {
-        res.status(500).send('Internal server error')
-        return
+        if (location) {
+            res.redirect(location)
+            return
+        }
     }
 
-    if (parts.length === 1) {
-        const thread = await channel.threads.fetch(parts[0])
-        if (!thread?.url) {
-            res.status(500).send('Internal server error')
-            return
-        }
-
-        res.redirect(thread.url)
-    } else {
-        const thread = await channel.threads.fetch(parts[0])
-        if (!thread) {
-            res.status(500).send('Internal server error')
-            return
-        }
-
-        const message = await thread.messages.fetch(parts[1])
-        if (!message.url) {
-            res.status(500).send('Internal server error')
-            return
-        }
-
-        res.redirect(message.url)
-    }
+    res.status(500).send('Internal server error')
 })
 
 const port = process.env.PORT ? parseInt(process.env.PORT) : 8080
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 })
-
-const SUPPORT_CHANNEL_ID = process.env.SUPPORT_CHANNEL_ID
-
-const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages],
-})
-
-client.on('ready', async () => {
-    console.log(`Logged in as ${client.user!.tag}!`)
-})
-
-client.on('interactionCreate', async interaction => {
-    if (
-        interaction.type === InteractionType.MessageComponent &&
-        interaction.componentType === ComponentType.Button &&
-        interaction.customId === 'close'
-    ) {
-        const thread = interaction.message.channel as AnyThreadChannel
-
-        await interaction.update({
-            components: [createActionButtonRow('reopen')],
-        })
-        await thread.setArchived(true)
-    }
-
-    if (
-        interaction.type === InteractionType.MessageComponent &&
-        interaction.componentType === ComponentType.Button &&
-        interaction.customId === 'reopen'
-    ) {
-        const thread = interaction.message.channel as AnyThreadChannel
-
-        await thread.setArchived(false)
-        await interaction.update({
-            components: [createActionButtonRow('close')],
-        })
-    }
-})
-
-function createActionButtonRow(button: 'close' | 'reopen'): ActionRowBuilder<ButtonBuilder> {
-    const row = new ActionRowBuilder<ButtonBuilder>()
-
-    row.addComponents(
-        button === 'close'
-            ? new ButtonBuilder().setLabel('Close').setCustomId('close').setStyle(ButtonStyle.Danger).setEmoji('🔒')
-            : new ButtonBuilder().setLabel('Reopen').setCustomId('reopen').setStyle(ButtonStyle.Primary).setEmoji('🔓'),
-        new ButtonBuilder()
-            .setLabel('Docs')
-            .setStyle(ButtonStyle.Link)
-            .setEmoji('📚')
-            .setURL('https://docs.sourcegraph.com/'),
-        new ButtonBuilder()
-            .setLabel('YouTube')
-            .setStyle(ButtonStyle.Link)
-            .setEmoji('📺')
-            .setURL('https://www.youtube.com/c/sourcegraph'),
-        new ButtonBuilder()
-            .setLabel('Status')
-            .setStyle(ButtonStyle.Link)
-            .setEmoji('🧭')
-            .setURL('https://sourcegraphstatus.com/')
-    )
-
-    return row
-}
-
-client.on('threadCreate', async interaction => {
-    if (interaction.parentId !== SUPPORT_CHANNEL_ID) {
-        return
-    }
-
-    await interaction.send({
-        content: `Hey <@${interaction.ownerId}>, thanks for reaching out! The resources below might be useful to you.`,
-        components: [createActionButtonRow('close')],
-    })
-
-    const starter = await interaction.fetchStarterMessage()
-
-    if (starter) {
-        externalResourceQueue.push({
-            external_id: interaction.id,
-            author: {
-                external_id: starter.author.id,
-                name: starter.author.username,
-                image_url: starter.author.displayAvatarURL({ size: 512 }),
-                locale: 'en',
-                fields: [],
-            },
-            created_at: new Date(interaction.createdTimestamp ?? Date.now()).toISOString(),
-            message: starter.content,
-            internal_note: false,
-            allow_channelback: true,
-            fields: [
-                {
-                    id: 'subject',
-                    value: interaction.name,
-                },
-            ],
-        })
-    } else {
-        console.error('no starter!')
-    }
-})
-
-client.on('messageCreate', async interaction => {
-    if (
-        // Messages by our bot user
-        interaction.author.id === client.user!.id ||
-        // Messages not in a thread
-        !interaction.channel.isThread() ||
-        // Messages that are thread starters
-        interaction.type === MessageType.ThreadStarterMessage ||
-        // Messages not in the forum we control
-        interaction.channel.parentId !== SUPPORT_CHANNEL_ID ||
-        // Messages that are thread starters but not label thread starters for some reason
-        (await interaction.channel.fetchStarterMessage())?.id === interaction.id
-    ) {
-        return
-    }
-
-    externalResourceQueue.push({
-        external_id: `${interaction.channelId}-${interaction.id}`,
-        thread_id: interaction.channelId,
-        author: {
-            external_id: interaction.author.id,
-            name: interaction.author.username,
-            image_url: interaction.author.displayAvatarURL({ size: 512 }),
-            locale: 'en',
-            fields: [],
-        },
-        created_at: new Date(interaction.createdTimestamp).toISOString(),
-        message: interaction.content,
-        internal_note: false,
-        allow_channelback: true,
-    })
-})
-
-client.login(process.env.DISCORD_TOKEN)
